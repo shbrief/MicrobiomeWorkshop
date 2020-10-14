c7c19cdc7f33ca8578d00cf5adbcdfa4dab9f2f3
Sehyun Oh <shbrief@hotmail.com>
2020-10-14 15:37:25 +0000

    minor updates

--- inst/scripts/Biobakery_to_ExpressionSet.R
+++ inst/scripts/Biobakery_to_ExpressionSet.R
@@ -8,8 +8,8 @@ sampleName <- gsub("_R1.fastq.gz", "", basename(inputs$V1))
 ## Setup the directory structure
 dataset_name <- "test"
 dat_dir <- file.path("data", dataset_name)
-if (!dir.exists(dat_dir)) dir.create(dat_dir)
-dir_list <- c("genefamilies_relab", "marker_abundance", "marker_presence", "metadata", 
+if (!dir.exists(dat_dir)) dir.create(dat_dir, recursive = TRUE)
+dir_list <- c("genefamilies_relab", "marker_abundance", "marker_presence", "metadata",
               "metaphlan_bugs_list", "pathabundance_relab", "pathcoverage")
     for (i in seq_along(dir_list)) {
   target_dir <- file.path(dat_dir, dir_list[i])
@@ -25,8 +25,18 @@ outputs <- avworkflow_files()
 
 for (i in seq_along(dir_list)) {
   ind <- grep(dir_list[i], outputs$file)
+  sampleOnly <- sapply(ind, function(x) {
+    unlist(stringr::str_split(outputs$file[x], "_"))[1] %in% sampleName
+  })
+
+  if (length(sampleOnly) == 0) {
+    print(paste("No output is available for", dir_list[i]))
+    next
+  } else {ind <- ind[sampleOnly]}
+
   for (j in ind) {
-    gsutil_cp(outputs$path[j], 
-              destination = file.path(dat_dir, dir_list[i]))
+    fname <- unlist(stringr::str_split(outputs$file[j], "_"))[1]
+    gsutil_cp(outputs$path[j],
+              destination = file.path(dat_dir, dir_list[i], paste0(fname, ".tsv")))
   }
 }
